cde43bb9dc1dd985e3586efa256855b93ba67b6b
JorgeLopes-BytePitch <jorge.lopes@bytepitch.com>
2024-04-24 16:12:46 +0100

    test(e2e): update

--- frontend/test/e2e/synpress.config.js
+++ frontend/test/e2e/synpress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     ...baseConfig.e2e,
     baseUrl: 'http://localhost:5173',
     specPattern: 'test/e2e/specs/**/*spec.{js,jsx,ts,tsx}',
-    supportFile: 'test/e2e/support.js',
+    supportFile: 'test/support.js',
     screenshotsFolder: 'test/e2e/screenshots',
     videosFolder: 'test/e2e/videos'
   },
